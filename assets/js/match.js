86d72892d325d319ba916b6825ae059e98de47a6
Andrew Kennedy <andrew.kennedy27@outlook.com>
2022-10-16 20:33:42 +0100

    reconfigured createGrid to add images to html

--- assets/js/match.js
+++ assets/js/match.js
@@ -1,44 +1,85 @@
-// Create 12 block grid
-function createGrid() {
-  const columns = 3;
-  const rows = 4;
+const selectors = {
+  boardContainer: document.querySelector(".game__board_container"),
+  board: document.querySelector(".game__board"),
+  win: document.querySelector(".win"),
+};
 
-  const grid = document.createElement("div");
-  grid.className = "game__board__wrapper";
-  for (let i = 0; i < columns; ++i) {
-    let column = document.createElement("div"); // create column
-    column.className = "game__board__column";
-    for (let j = 0; j < rows; ++j) {
-      let row = document.createElement("div"); // create row
-      row.className = "game__board__box";
-      column.appendChild(row); // append row in column
-    }
-    grid.appendChild(column); // append column inside grid
+const pickRandom = (array, items) => {
+  const clonedArray = [...array];
+  const randomPicks = [];
+
+  for (let i = 0; i < items; i++) {
+    const randomIndex = Math.floor(Math.random() * clonedArray.length);
+    randomPicks.push(clonedArray[randomIndex]);
+    clonedArray.splice(randomIndex, 1);
   }
-  document.body.appendChild(grid);
+  return randomPicks;
+};
+
+const shuffle = (array) => {
+  const clonedArray = [...array];
+
+  for (let index = clonedArray.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    const original = clonedArray[index];
+
+    clonedArray[index] = clonedArray[randomIndex];
+    clonedArray[randomIndex] = original;
+  }
+
+  return clonedArray;
+};
+
+// Create game grid
+function createGrid() {
+  const dimensions = selectors.board.getAttribute("data-dimension");
+  // Array of image names from assets/images
+  const fruitNames = [
+    "banana",
+    "blackberry-light",
+    "black-cherry",
+    "green-grape",
+    "lime",
+    "orange",
+    "raspberry",
+    "red-apple",
+    "red-cherry",
+    "red-grape",
+    "strawberry",
+  ];
+
+  const imgArray = pickRandom(fruitNames, (dimensions * dimensions) / 2);
+  console.log(imgArray);
+  const imgUrls = shuffle([...imgArray, ...imgArray]);
+  const imgBaseSrc = "assets/images/";
+  const imgExtension = ".png";
+
+  const gameCards = `
+        <div class="game__board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${imgUrls
+              .map(
+                (imgUrl) => `
+                <div class="game__board__column">
+                    <div class="game__board__box"><img class="game__board__image" src="${imgBaseSrc}${imgUrl}${imgExtension}" /></div>
+
+
+                </div>
+            `
+              )
+              .join("")}
+       </div>
+    `;
+
+  const parser = new DOMParser().parseFromString(gameCards, "text/html");
+  console.log(parser);
+
+  selectors.board.replaceWith(parser.querySelector(".game__board"));
 }
-
-addEventListener("DOMContentLoaded", (event) => {
-  createGrid();
-});
-
-// Array of image names from assets/images
-const fruitName = [
-  "banana",
-  "blackberry-light",
-  "black-cherry",
-  "green-grape",
-  "lime",
-  "orange",
-  "raspberry",
-  "red-apple",
-  "red-cherry",
-  "red-grape",
-  "strawberry",
-];
-
-// Iterate over image array and assign to image src variable in HTML
 
 // onclick make image visible with limit of 2 images visible at a time
 
 // if image name matches, images stay visible
+
+addEventListener("DOMContentLoaded", (event) => {
+  createGrid();
+});
